3a0e1e4c84c82bc60d4f4dfaf9553d556efec4c3
Patrick <pdn6991@gmail.com>
2021-06-18 14:19:50 -0400

    Added route and method for query params

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,2 +1,6 @@
 class PhrasesController < ApplicationController
+  def query
+    print_phrase = params[:phrase].upcase
+    render json: {message: print_phrase.as_json}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/phrase", controller: "phrases", action: "query"
+  get "/phrase/:url_phrase", controller: "phrases", action: "url_segment"
 end
