8560a07c1a909d116ea5cdfac551617555ccc05a
Patrick <pdn6991@gmail.com>
2021-06-18 14:25:06 -0400

    Added route and method for url segment params

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -3,4 +3,9 @@ class PhrasesController < ApplicationController
     print_phrase = params[:phrase].upcase
     render json: {message: print_phrase.as_json}
   end
+
+  def url_segment
+    print_phrase = params[:url_phrase]
+    render json: {message: print_phrase.as_json}
+  end
 end
